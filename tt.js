d67a0c65992d9f16507eb4930b0f51c3a9a1b812
thuluu@gmail.com <thuluu906@gmail.com>
2023-07-03 13:50:09 +0700

    add js

---
+++ tt.js
@@ -0,0 +1,31 @@
+
+/*let Name = "Apple";//let là từ khóa định nghĩa biến
+function MyFunction(){ //funtion là từ khóa báo cho trình duyệt tạo một hàm function
+  let num = 45;
+  console.log(Name);
+  console.log(num);
+
+ }
+ MyFunction();*/
+
+
+ //lưu kiểu  kiểu dữ liệu mảng
+/*let places= ["GFG","Computer","Hello"];
+console.log(places);*/
+ 
+
+ // Nó lưu trữ dữ liệu đối tượng (các đối tượng được biểu diễn chủ yếu theo cách dưới đây) 
+
+//let emailKey = "email";
+let Student ={
+    name: "th",
+    age: 18,
+    address: "NA",
+    
+};
+
+let b= "address";
+
+Student[b]="HCM"
+
+console.log(Student["address"]);
